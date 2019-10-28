fac5dbf4a9d0e3906a720e47c6b13818be89ee85
mwalimuianT <mwalimuiant@gmail.com>
2019-10-28 14:29:12 +0300

    commented on the container in styles.css to remove the background-image

--- custom.js
+++ custom.js
@@ -1,7 +1,6 @@
 var submission = function() {
 var maleNames = ['Kwasi','Kwandwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']    
-var femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Yaa','Afua','Ama']
-var femaleNames = ['Akosua']
+var femaleNames = ['Akosua', 'Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
 
 var daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
   
